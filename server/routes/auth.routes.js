a4b93862b4355a436ad4c2d0716c9c1745ee6790
Daniil Ognevoy <yovengowo@gmail.com>
2022-10-12 11:07:06 +0300

    add signup method to auth routes

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+const tokenService = require('../services/token.service');
+const router = express.Router({ mergeParams: true });
+
+router.post('signUp', [
+  check('email', 'Incorrect email').isEmail(),
+  check('password', 'Password must be at least 8 characters long').isLength({ min: 8 }),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          error: {
+            message: 'INVALID_DATA',
+            code: 400,
+            errors: errors.array(),
+          },
+        });
+      }
+
+      const { email, password } = req.body;
+
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({
+          error: {
+            message: 'EMAIL_EXISTS',
+            code: 400,
+          },
+        });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      const newUser = await User.create({
+        ...req.body,
+        password: hashedPassword,
+        tasks: [],
+      });
+
+      const tokens = tokenService.generate({ _id: newUser._id });
+      await tokenService.save(newUser._id, tokens.refreshToken);
+
+      res.status(201).send({ ...tokens, userId: newUser._id });
+    } catch (error) {
+      res.status(500).json({ message: 'An error has occurred on the server. Try later' });
+    }
+  },
+]);
