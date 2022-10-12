5188bbcf3ab03ad6d081dc8f81b181c61dfe5fb7
Daniil Ognevoy <yovengowo@gmail.com>
2022-10-12 11:17:03 +0300

    add signin method to auth routes

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -50,3 +50,50 @@ router.post('signUp', [
     }
   },
 ]);
+
+router.post('signInWithPassword', [
+  check('email', 'Email is incorrect').normalizeEmail().isEmail(),
+  check('password', 'Password is required').exists(),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          error: {
+            message: 'INVALID_DATA',
+            code: 400,
+          },
+        });
+      }
+
+      const { email, password } = req.body;
+
+      const existingUser = await User.findOne({ email });
+      if (!existingUser) {
+        return res.status(400).send({
+          error: {
+            message: 'EMAIL_NOT_FOUND',
+            code: 400,
+          },
+        });
+      }
+
+      const isPasswordEqual = await bcrypt.compare(password, existingUser.password);
+      if (!isPasswordEqual) {
+        return res.status(400).send({
+          error: {
+            message: 'INVALID_PASSWORD',
+            code: 400,
+          },
+        });
+      }
+
+      const tokens = tokenService.generate({ _id: existingUser._id });
+      await tokenService.save(existingUser._id, tokens.refreshToken);
+
+      res.status(200).send({ ...tokens, userId: existingUser._id });
+    } catch (error) {
+      res.status(500).json({ message: 'An error has occurred on the server. Try later' });
+    }
+  },
+]);
