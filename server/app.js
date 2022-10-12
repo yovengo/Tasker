9901c1cccbad33092bdd711fcd20c34b6ab3f454
Daniil Ognevoy <yovengowo@gmail.com>
2022-10-12 10:29:41 +0300

    add index for routes

--- server/app.js
+++ server/app.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 const config = require('config');
 const chalk = require('chalk');
 const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+app.use('/api', routes);
 
 const PORT = config.get('port') ?? 8080;
 
