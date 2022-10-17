ea644d0417d39826324be1572e633f2768afa952
Daniil Ognevoy <yovengowo@gmail.com>
2022-10-17 14:05:17 +0300

    hotfix routes index file

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,6 @@ const router = express.Router({ mergeParams: true });
 
 router.use('/auth', require('./auth.routes'));
 router.use('/user', require('./user.routes'));
-router.use('/task', require('task.routes'));
+router.use('/task', require('./task.routes'));
 
 module.exports = router;
