6b8bb5a8240f2f481813122ecb3e56655fb854d4
Daniil Ognevoy <yovengowo@gmail.com>
2022-10-20 17:32:21 +0300

    add browser router in project

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,13 +4,16 @@ import './index.css';
 import App from './app/App';
 import { Provider } from 'react-redux';
 import store from './app/store/createStore';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
